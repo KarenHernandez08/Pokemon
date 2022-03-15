a0669f5c35e9a228be810ac783d258ca1cd3b7cc
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-03-15 13:59:24 -0600

    Pokedex
    práctica final

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Pokemon
+# Pokedex
  Uso de la api de pokemon
+
+Creación de un Pokedex con la tecnología de HTML, CSS Y JS
+ fue de mucha ayuda este [video](https://www.youtube.com/watch?v=i8Zfq87HoGg&t=941s&ab_channel=Programaci%C3%B3nAccesible)
+
+ ## Estas son las capturas de como se ve desarrollado el pokedex
+
+ !()[https://github.com/KarenHernandez08/Pokemon/blob/main/pokeapi/imagenes/index.PNG]
+ !()[https://github.com/KarenHernandez08/Pokemon/blob/main/pokeapi/imagenes/index2.PNG]
+ !()[https://github.com/KarenHernandez08/Pokemon/blob/main/pokeapi/imagenes/no-encontrado.PNG]
--- pokeapi/pokemonLauncX.js
+++ pokeapi/pokemonLauncX.js
@@ -1,21 +1,30 @@
-const fetchPokemon =() => {
+
+
+const fetchPokemon = () => {
     const pokeName= document.getElementById("pokeName");
     let pokeInput =pokeName.value.toLowerCase();
     const url =`https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+    
     fetch(url).then((res)=>{
         if(res.status !="200"){
             console.log(res);
-            pokeImage("/imagenes/pikachu.png");
+            pokeImage("/pokeapi/imagenes/pikachu.png");
         }
         else{
             return res.json();
+            
+
         }
          
     }).then((data)=>{
+        if(data){
         console.log(data);
         let pokeimg = data.sprites.front_default;
         console.log(pokeimg);
         pokeImage(pokeimg);
+        let
+
+        }
 
     })
 }
@@ -27,7 +36,17 @@ const pokeImage= (url) =>{
     pokeImage.src= url;
 
 }
+const pokename=()=>{
+    const p = document.getElementById("data-poke-name");
+    
+}
 
-pokeimage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png")
+
+
+
+
+
+
+  
 
 
